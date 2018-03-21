9dcebeb81c94d8a416946d2d7e7204056635ae9b
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-03-21 22:40:07 +0100

    Add parench images

--- src/detailedSlice.js
+++ src/detailedSlice.js
@@ -3,11 +3,15 @@ import './detailedSlice.css'
 
 class DetailedSlice extends Component {
   render () {
+    const frameImage = require(`../public/images/parench/parench-0${this.props.frame + 50}.JPG`)
     return (
       <div className='detailed-slice'>
         <p className='detailed-slice__text'>
           Frame {this.props.frame}
         </p>
+        <div className='detailed-slice__image-container'>
+          <img src={frameImage} className='detailed-slice__image' alt='my'/>
+        </div>
       </div>
     )
   }
