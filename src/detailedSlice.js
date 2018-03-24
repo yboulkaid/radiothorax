a1a0a0fd4d0929be946da388f794e13e0dbadffd
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-03-24 20:27:52 +0100

    Add csv organs

--- src/detailedSlice.js
+++ src/detailedSlice.js
@@ -3,6 +3,22 @@ import Label from './label.js'
 import { Stage } from 'react-konva';
 import './detailedSlice.css'
 
+const csvParse = require('csv-parse/lib/sync');
+const organList = require('../public/organes.csv')
+const base64File = organList.split(',')[1]
+const decodedCsv = atob(base64File)
+const organs = csvParse(decodedCsv, {delimiter: ';', columns: true})
+
+const organLabels=organs.map(function (organ) {
+  return (
+    <Label
+      key={organ.organe}
+      from={{x: organ.labelX, y: organ.labelY}}
+      to={{x: organ.x, y: organ.y}}
+      text={organ.organe}/>
+  )
+})
+
 class DetailedSlice extends Component {
   render () {
     const frameImage = require(`../public/images/parench/parench-${this.props.frame + 50}.JPG`)
@@ -13,8 +29,8 @@ class DetailedSlice extends Component {
         </p>
         <div className='detailed-slice__image-container'>
           <img src={frameImage} className='detailed-slice__image' alt='my'/>
-          <Stage width={400} height={400} className='label'>
-            <Label from={{x: 100, y: 100}} to={{x: 200, y: 200}} text='Organe 1'/>
+          <Stage width={500} height={500} className='label'>
+            { organLabels }
           </Stage>
         </div>
       </div>
