df04618ec9088b94babd3fd5fa8afe8061e1a4df
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-03-24 21:07:37 +0100

    Toggle visibility of organ labels

--- src/detailedSlice.js
+++ src/detailedSlice.js
@@ -9,17 +9,28 @@ const base64File = organList.split(',')[1]
 const decodedCsv = atob(base64File)
 const organs = csvParse(decodedCsv, {delimiter: ';', columns: true})
 
-const organLabels=organs.map(function (organ) {
-  return (
-    <Label
-      key={organ.organe}
-      from={{x: organ.labelX, y: organ.labelY}}
-      to={{x: organ.x, y: organ.y}}
-      text={organ.organe}/>
-  )
-})
+
 
 class DetailedSlice extends Component {
+  isVisibleFor (frameStart, frameEnd) {
+    return this.props.frame >= frameStart && this.props.frame <= frameEnd
+  }
+
+  organLabels () {
+    return organs.map((organ) => {
+
+      if (this.isVisibleFor(organ.frameStart, organ.frameEnd)) {
+        return (
+          <Label
+            key={organ.organe}
+            from={{x: organ.labelX, y: organ.labelY}}
+            to={{x: organ.x, y: organ.y}}
+            text={organ.organe}/>
+        )
+      }
+    })
+  }
+
   render () {
     const frameImage = require(`../public/images/parench/parench-${this.props.frame + 50}.JPG`)
     return (
@@ -30,7 +41,7 @@ class DetailedSlice extends Component {
         <div className='detailed-slice__image-container'>
           <img src={frameImage} className='detailed-slice__image' alt='my'/>
           <Stage width={500} height={500} className='label'>
-            { organLabels }
+            { this.organLabels() }
           </Stage>
         </div>
       </div>
