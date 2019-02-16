19a9bb2238fe8445e3195b1687b2873a8b903e39
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2019-02-17 00:51:19 +0100

    Remove konva

--- part1/src/detailedSlice.js
+++ part1/src/detailedSlice.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import LabelSet from './labelSet.js'
-import { Stage } from 'react-konva';
 import './detailedSlice.css'
 
 class DetailedSlice extends Component {
@@ -14,20 +13,18 @@ class DetailedSlice extends Component {
         </p>
         <div className='detailed-slice__image-container' id='scrollable'>
           <img src={frameImage} className='detailed-slice__image' alt='my'/>
-          <Stage width={500} height={500} className='label'>
-            <LabelSet
-              frame={this.props.frame}
-              color='red'
-              fileName='VertebresAxial.fcsv.csv'
-              visible={this.props.captionSet === 'set1'}
-            />
-            <LabelSet
-              frame={this.props.frame}
-              color='yellow'
-              fileName='organes2.csv'
-              visible={this.props.captionSet === 'set2'}
-            />
-          </Stage>
+          <LabelSet
+            frame={this.props.frame}
+            color='red'
+            fileName='VertebresAxial.fcsv.csv'
+            visible={this.props.captionSet === 'set1'}
+          />
+          <LabelSet
+            frame={this.props.frame}
+            color='yellow'
+            fileName='organes2.csv'
+            visible={this.props.captionSet === 'set2'}
+          />
         </div>
       </div>
     )
