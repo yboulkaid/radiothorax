b9e90ef2057f97cc20358f061c39fcd123a08294
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2019-02-17 01:51:23 +0100

    Better presentation for labels

--- part1/src/label.js
+++ part1/src/label.js
@@ -11,16 +11,6 @@ class Label extends Component {
       <div className='label' style={styles}>
         {this.props.text}
       </div>
-      // <Layer>
-      //   <Text
-      //     text={this.props.text}
-      //     x={Math.floor(this.props.from.x) - 600}
-      //     y={Math.floor(this.props.from.y) - 500}
-      //     align={'left'}
-      //     fontStyle={'bold'}
-      //     fill={this.props.color}
-      //   />
-      // </Layer>
     );
   }
 }
