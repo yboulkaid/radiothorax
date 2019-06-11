89f3f357af5bb76aa0612da2ac710ab9386013e6
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-11 20:41:11 +0200

    Extract Viewer component

---
+++ part1/src/viewer.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import MainSlider from './mainSlider.js'
+import SliceView from './sliceView.js'
+import DetailedSlice from './detailedSlice.js'
+import LabelSelector from './labelSelector.js'
+import './viewer.css'
+
+class Viewer extends Component {
+  constructor () {
+    super()
+    this.setFrame = this.setFrame.bind(this)
+    this.setCaption = this.setCaption.bind(this)
+    this.state = {
+      frame: 0,
+      totalFrames: 70,
+      caption: 'set1'
+    }
+  }
+
+  setFrame (frame) {
+    this.setState(
+      {
+        frame: frame,
+        totalFrames: this.state.totalFrames,
+        caption: this.state.caption
+      }
+    )
+  }
+
+  setCaption (caption) {
+    this.setState(
+      {
+        frame: this.state.frame,
+        totalFrames: this.state.totalFrames,
+        caption: caption
+      }
+    )
+  }
+
+  componentDidMount () {
+    document.getElementById('scrollable').addEventListener(
+      'wheel', (event) => { this.reactToScrollWheel(event) }
+      , { capture: false, passive: false });
+  }
+
+  reactToScrollWheel (event) {
+    var newFrame = this.state.frame
+    if (event.deltaY > 0) {
+      newFrame = Math.max(this.state.frame - 1, 0)
+    } else {
+      newFrame = Math.min(this.state.frame + 1, this.state.totalFrames)
+    }
+    this.setFrame(newFrame)
+    event.preventDefault();
+  }
+
+  render () {
+    const sideView1 = require('../public/images/radio/BoneAxial/sides/side-1.jpg')
+    const sideView2 = require('../public/images/radio/BoneAxial/sides/side-2.jpg')
+
+    return (
+      <div className='App-viewer'>
+        <div className='App-sidebar'>
+          <SliceView frame={this.state.frame} totalFrames={this.state.totalFrames} image={sideView1} />
+          <SliceView frame={this.state.frame} totalFrames={this.state.totalFrames} image={sideView2} />
+        </div>
+
+        <div className='App-detailed-slice'>
+          <DetailedSlice frame={this.state.frame} captionSet={this.state.caption} />
+        </div>
+
+        <div className='App-footer'>
+          <MainSlider frame={this.state.frame} totalFrames={this.state.totalFrames} setFrameHandler={this.setFrame} />
+          <LabelSelector selectedValue='orange' setCaptionHandler={this.setCaption}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Viewer
