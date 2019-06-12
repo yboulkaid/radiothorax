e36d1b1d481569baacef2cc61f8747e0b315cd8d
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-12 20:02:13 +0200

    Use fetch to get organs

--- part1/src/labelSet.js
+++ part1/src/labelSet.js
@@ -2,43 +2,54 @@ import React, { Component } from 'react'
 import Label from './label.js'
 
 class LabelSet extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      organs: [],
+      filename: this.props.fileName
+    }
+
+    this.getOrgansLabels = this.getOrgansLabels.bind(this)
+    this.organToLabel = this.organToLabel.bind(this)
+    this.getOrgansLabels()
+  }
+
   isVisibleFor (frameStart, frameEnd) {
     frameEnd += 1 // Because of weird data input?
     return this.props.visible && this.props.frame >= frameStart && this.props.frame <= frameEnd
   }
 
-  organs (fileName) {
+  getOrgansLabels() {
     const csvParse = require('csv-parse/lib/es5/sync');
-    const organList = require(`../public/captions/${fileName}`)
-    const base64File = organList.split(',')[1]
-    const decodedCsv = atob(base64File)
-    const organs = csvParse(decodedCsv, {delimiter: ';', columns: true})
-    return organs
+    const fileUrl = `/captions/${this.props.fileName}`
+    fetch(fileUrl)
+      .then(response => response.text())
+      .then(csvString => csvParse(csvString, {delimiter: ';', columns: true}))
+      .then(csv => this.state.organs = csv)
   }
 
-  organLabels (fileName) {
-    return this.organs(fileName).map((organ) => {
-      let scaleFactor = 2
-      let scaledX = Math.round(organ.x / scaleFactor)
-      let scaledY = Math.round(organ.y / scaleFactor)
-      if (this.isVisibleFor(organ.frameStart, organ.frameEnd)) {
-        return (
-          <Label
-            key={organ.label}
-            from={{x: scaledX, y: scaledY}}
-            to={{x: scaledX, y: scaledY}}
-            text={organ.label}
-            color={this.props.color}/>
-        )
-      } else {
-        return []
-      }
-    })
+  organToLabel (organ) {
+    let scaleFactor = 2
+    let scaledX = Math.round(organ.x / scaleFactor)
+    let scaledY = Math.round(organ.y / scaleFactor)
+    if (this.isVisibleFor(organ.frameStart, organ.frameEnd)) {
+      return (
+        <Label
+          key={organ.label}
+          from={{x: scaledX, y: scaledY}}
+          to={{x: scaledX, y: scaledY}}
+          text={organ.label}
+          color={this.props.color}/>
+      )
+    } else {
+      return []
+    }
   }
 
   render () {
-    return (
-      this.organLabels(this.props.fileName)
+    console.log(this.state.organs)
+    return this.state.organs.map(
+      (organ) => this.organToLabel(organ)
     )
   }
 }
